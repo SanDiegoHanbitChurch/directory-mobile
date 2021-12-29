efa797042ad6c5211c0ae870addd4c35b4a659e1
Justin Yum <justinyum98@gmail.com>
2021-12-29 14:49:26 -0800

    installed and initialized firebase

---
+++ src/firebase/index.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyATiJpM14Y8HPRLWbaL_uNhsVRjy7uqjNo',
+  authDomain: 'hanbit-directory-dev.firebaseapp.com',
+  projectId: 'hanbit-directory-dev',
+  storageBucket: 'hanbit-directory-dev.appspot.com',
+  messagingSenderId: '575510018711',
+  appId: '1:575510018711:web:f56b4085358482ee4547d2',
+  measurementId: 'G-KMQ9GC3BZJ',
+};
+
+initializeApp(firebaseConfig);
