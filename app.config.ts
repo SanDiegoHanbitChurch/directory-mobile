32e539d227a8a21d73030192e0811e28130f1793
Justin Yum <justinyum98@gmail.com>
2021-12-30 16:31:31 -0800

    moved oauth client ids to env file

---
+++ app.config.ts
@@ -0,0 +1,40 @@
+import { ExpoConfig } from '@expo/config';
+import 'dotenv/config';
+
+export default (): ExpoConfig => ({
+  name: 'directory-mobile',
+  slug: 'directory-mobile',
+  owner: 'san-diego-hanbit-church',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    bundleIdentifier: 'org.sdhanbit.mobile',
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#FFFFFF',
+    },
+    package: 'org.sdhanbit.mobile',
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+  scheme: 'hanbit',
+  extra: {
+    expoClientId: process.env.EXPO_CLIENT_ID,
+    androidClientId: process.env.ANDROID_CLIENT_ID,
+    iosClientId: process.env.IOS_CLIENT_ID,
+  },
+});
--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Constants from 'expo-constants';
 import { StatusBar } from 'expo-status-bar';
 import * as Google from 'expo-auth-session/providers/google';
 import { GoogleAuthProvider, signInWithCredential } from 'firebase/auth';
@@ -24,12 +25,9 @@ export default function SignInScreen() {
   const { signInWithGoogle } = useAuth();
 
   const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
-    expoClientId:
-      '575510018711-u15bc2aftkkegcj1s1mj4uje1v70m9re.apps.googleusercontent.com',
-    androidClientId:
-      '575510018711-oifl5jh2ddc3u621lfjnu5kte7pb8av3.apps.googleusercontent.com',
-    iosClientId:
-      '575510018711-ppeumetei06dan08nkdvov3e2ht1ot0t.apps.googleusercontent.com',
+    expoClientId: Constants.manifest?.extra?.expoClientId,
+    androidClientId: Constants.mainfest?.extra?.androidClientId,
+    iosClientId: Constants.manifest?.extra?.iosClientId,
   });
 
   React.useEffect(() => {
