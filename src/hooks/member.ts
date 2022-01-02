b4e6b72f5d91cc580b352dc0589f515319ac6bb7
Justin Yum <justinyum98@gmail.com>
2022-01-01 21:13:27 -0800

    Implement infinite scrolling (#6)
    * added infinite scroll
    
    * removed unused import
    
    * moved api base url to env

--- src/hooks/member.ts
+++
@@ -1,8 +0,0 @@
-import { AxiosError } from 'axios';
-import { User } from 'firebase/auth';
-import { useQuery } from 'react-query';
-
-import { getAllMembers, Member } from '../api/member';
-
-export const useMembers = (currentUser: User) =>
-  useQuery<Member[], AxiosError>('members', () => getAllMembers(currentUser));
